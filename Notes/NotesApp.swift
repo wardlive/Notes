c400b8515e5d83b29691307d08dacd5f7132c749
Pavel Neprin <41507524+neprin@users.noreply.github.com>
2022-10-21 03:40:30 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Pavel Neprin on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
